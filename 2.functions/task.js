c3a1f0b802b3aa69f79544221bd504750c1a738a
olcikfarm-design <olcikfarm@gmail.com>
2026-09-12 19:31:51 +0500

    second test.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -22,21 +22,54 @@ function getArrayParams(...arr) {
   return { min, max, avg };
 }
 
-
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  
+  return arr.reduce((sum, current) => sum + current, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) return 0;
+  
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+  if (arr.length === 0) return 0;
+  
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement += 1;
+    }
+  }
+
+  if (countEvenElement === 0) return 0;
+
+  return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
