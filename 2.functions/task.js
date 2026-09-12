039070a60cfbdf3aa58fd72c143e6636679d978f
olcikfarm-design <olcikfarm@gmail.com>
2026-09-12 19:22:59 +0500

    first test.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,27 @@
 function getArrayParams(...arr) {
+  if (arr.length === 0) {
+    return { min: undefined, max: undefined, avg: 0 };
+  }
 
-  return { min: min, max: max, avg: avg };
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  const avg = Number((sum / arr.length).toFixed(2));
+
+  return { min, max, avg };
 }
+
 
 function summElementsWorker(...arr) {
 
