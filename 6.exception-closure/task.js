140d3c559ac2437fadc9054c50b56d37782d84f8
olcikfarm-design <olcikfarm@gmail.com>
2026-09-14 22:27:13 +0500

    First task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,17 @@
-﻿
+﻿function parseCount(value) {
+  const result = Number.parseFloat(value);
+
+  if (Number.isNaN(result)) {
+    throw new Error('Невалидное значение');
+  }
+
+  return result;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
