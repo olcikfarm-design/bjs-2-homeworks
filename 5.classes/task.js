55a7098570253c3245e7c53fa430a9f15f238ce5
olcikfarm-design <olcikfarm@gmail.com>
2026-09-14 21:14:46 +0500

    Second task.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -61,3 +61,61 @@ class DetectiveBook extends Book {
     this.type = "detective";
   }
 }
+
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    for (let i = 0; i < this.books.length; i++) {
+      if (this.books[i][type] === value) {
+        return this.books[i];
+      }
+    }
+    return null; 
+  }
+
+  giveBookByName(bookName) {
+    const index = this.books.findIndex(book => book.name === bookName);
+    
+    if (index !== -1) {
+      return this.books.splice(index, 1)[0];
+    }
+    
+    return null; 
+  }
+}
+
+const myLibrary = new Library("Городская центральная библиотека");
+
+myLibrary.addBook(new DetectiveBook("Артур Конан Дойл", "Приключения Шерлока Холмса", 2019, 500));
+myLibrary.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
+myLibrary.addBook(new Magazine("Мурзилка", 1924, 60));
+
+const book1919 = new NovelBook("Александр Блок", "Двенадцать", 1919, 32);
+myLibrary.addBook(book1919);
+console.log("Найдена книга 1919 года:", myLibrary.findBookBy("releaseDate", 1919).name); 
+
+const issuedBook = myLibrary.giveBookByName("Пикник на обочине");
+console.log("Выдана книга:", issuedBook.name);
+console.log("Количество книг в библиотеке после выдачи:", myLibrary.books.length); 
+
+
+issuedBook.state = 25;
+console.log("Состояние выданной книги после повреждения:", issuedBook.state); 
+
+issuedBook.fix(); // 25 * 1.5 = 37.5
+console.log("Состояние выданной книги после восстановления:", issuedBook.state); 
+
+myLibrary.addBook(issuedBook);
+console.log("Количество книг в библиотеке после возврата:", myLibrary.books.length); 
+
+console.log("Есть ли 'Пикник на обочине' в библиотеке?", myLibrary.findBookBy("name", "Пикник на обочине") !== null); 
